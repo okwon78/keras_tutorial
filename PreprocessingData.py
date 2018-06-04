e0bbdc55fa4b38a2fa96f36797f2e81c485da044
Kwon <okwon78@gmail.com>
2018-06-04 17:33:12 +0900

    ignore training data and model file

--- PreprocessingData.py
+++ PreprocessingData.py
@@ -35,4 +35,34 @@ def training_data():
     return train_samples, train_labels
 
 
+def testing_data():
+    test_labels = list()
+    test_samples = list()
+
+    for i in range(1000):
+        random_younger = randint(13, 64)
+        test_samples.append(random_younger)
+        test_labels.append(0)
+
+        random_older = randint(65, 100)
+        test_samples.append(random_older)
+        test_labels.append(1)
+
+    for i in range(50):
+        random_younger = randint(13, 64)
+        test_samples.append(random_younger)
+        test_labels.append(1)
+
+        random_older = randint(65, 100)
+        test_samples.append(random_older)
+        test_labels.append(0)
+
+    # numpy array is a requirement of keras for label and samples
+    test_labels = np.array(test_labels)
+    test_samples = np.array(test_samples)
+
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaled_train_samples = scaler.fit_transform(test_samples.reshape(-1,1))
+
+    return test_samples, test_labels
 
--- Sequential.py
+++ Sequential.py
@@ -3,63 +3,70 @@ from keras import Sequential
 from keras.layers import Dense, Activation
 
 import tensorflow as tf
+from keras.models import load_model, model_from_json
 from keras.optimizers import Adam
 
-from PreprocessingData import training_data
-
+from PreprocessingData import training_data, testing_data
 
 print(tf.__version__)
 print(keras.__version__)
 
-model = Sequential([
-    Dense(16, input_shape=(1,)),
-    Activation('relu'),
-    Dense(32),
-    Activation('relu'),
-    Dense(2),
-    Activation('softmax'),
-])
+try:
+    model = load_model('my_model.h5')
+    print(model.get_weights())
+except OSError:
+    train_samples, train_label = training_data()
+    one_hot_labels = keras.utils.to_categorical(train_label, num_classes=2)
 
-model.summary()
+    model = Sequential([
+        Dense(16, input_shape=(1,)),
+        Activation('relu'),
+        Dense(32),
+        Activation('relu'),
+        Dense(2),
+        Activation('softmax'),
+    ])
 
-model.compile(
-    Adam(lr=.0001),
-    loss='categorical_crossentropy',
-    metrics=['accuracy']
-)
+    model.summary()
 
-train_samples, train_label = training_data()
-one_hot_labels = keras.utils.to_categorical(train_label, num_classes=2)
+    model.compile(
+        Adam(lr=.0001),
+        loss='categorical_crossentropy',
+        metrics=['accuracy']
+    )
 
-model.fit(train_samples,
-          one_hot_labels,
-          # validation_split=0.1,
-          batch_size=20,
-          epochs=30,
-          shuffle=True,
-          verbose=1)
+    model.fit(train_samples,
+              one_hot_labels,
+              # validation_split=0.1,
+              batch_size=20,
+              epochs=30,
+              shuffle=True,
+              verbose=1)
+
+    model.save('my_model.h5')
+
+
+json_string = model.to_json()
+model_arch = model_from_json(json_string)
+
+test_samples, test_label = testing_data()
+one_hot_labels = keras.utils.to_categorical(test_label, num_classes=2)
+
+score = model.evaluate(test_samples, one_hot_labels, batch_size=10)
+
+print('\n', f'{model.metrics_names[0]}: {score[0]}')
+print(f'{model.metrics_names[1]}: {score[1]}', '\n')
 
 # predictions = model.predict(train_samples)
-predictions = model.predict_classes(train_samples)
+predictions = model.predict_classes(test_samples)
 
-# my_result = zip(predictions, train_label)
-#
-# Success = 0
-# Failure = 0
-#
-# for i in my_result:
-#     if i[0] == i[1]:
-#         Success += 1
-#     else:
-#         Failure += 1
-#
-# print(f"Success: {Success}, Failure {Failure}")
+Success = 0
+Failure = 0
 
-from sklearn.metrics import confusion_matrix
-import matplotlib.pyplot as plt
-import itertools
+for index, val in enumerate(predictions) :
+    if val == test_label[index]:
+        Success += 1
+    else:
+        Failure += 1
 
-
-cm = confusion_matrix(train_label, predictions)
-
-
+print(f"Success: {Success}, Failure {Failure}")
